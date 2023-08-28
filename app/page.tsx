faadc913b4d58b51746e0cf7ebacbc9f3f19ee32
IconicDexter <kennymwendwa67@gmail.com>
2023-08-28 08:25:13 +0300

    Initilised useState events

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 "use client";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
+import interactionPlugin, {
+  Draggable,
+  DropArg,
+} from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import { useState } from "react";
+
+interface Event {
+  title: string;
+  start: Date | string;
+  allDay: boolean;
+  id: number;
+}
 
 export default function Home() {
+  const [events, setEvents] = useState([
+    { title: "event 1", id: 1 },
+    { title: "event 2", id: 2 },
+    { title: "event 3", id: 3 },
+    { title: "event 4", id: 4 },
+    { title: "event 5", id: 5 },
+  ]);
   return (
     <>
       <nav className="flex justify-between mb-12 border-b border-violet-100 p-4">
@@ -18,7 +36,7 @@ export default function Home() {
               headerToolbar={{
                 left: "prev,next today",
                 center: "title",
-                right: "resourceTimelineWook, dayGridMonth,timeGridWeek",
+                right: "resourceTimelineWeek, dayGridMonth,timeGridWeek",
               }}
               events={{}}
               nowIndicator={true}
@@ -30,6 +48,20 @@ export default function Home() {
               // drop={{}}
               // eventClick={{}}
             />
+          </div>
+
+          <div
+            id="draggable-element"
+            className="ml-8 w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-50">
+            <div className="font-bold text-lg text-center">Drag Event</div>
+            {events.map((event) => (
+              <div
+                className="fc-event border-2 p-1 m-2 w-full rounded-md ml-auto text-center bg-white"
+                title={event.title}
+                key={event.id}>
+                {event.title}
+              </div>
+            ))}
           </div>
         </div>
       </main>
