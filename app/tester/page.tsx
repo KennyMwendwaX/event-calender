de0afc39d726a138b92e53b42fe6d5d98563a480
IconicDexter <kennymwendwa67@gmail.com>
2023-09-02 23:35:26 +0300

    Created a separate component for the Draggable Events

--- app/tester/page.tsx
+++ app/tester/page.tsx
@@ -11,8 +11,7 @@ import { EventSourceInput } from "@fullcalendar/core/index.js";
 import { useEffect, useState } from "react";
 import AddEventModal from "@/components/AddEventModal";
 import DeleteEventModal from "@/components/DeleteEventModal";
-import useLocalStorage from "@/hooks/useLocalStorage";
-import { title } from "process";
+import DragEvents from "@/components/DragEvents";
 
 export interface Event {
   title: string;
@@ -21,24 +20,7 @@ export interface Event {
   id: number;
 }
 
-export interface DraggableEvent {
-  id: number;
-  title: string;
-}
-
 export default function Tester() {
-  const [events, setEvents] = useState([
-    { title: "event 1", id: 1 },
-    { title: "event 2", id: 2 },
-    { title: "event 3", id: 3 },
-    { title: "event 4", id: 4 },
-    { title: "event 5", id: 5 },
-  ]);
-  const [eventInput, setEventInput] = useState("");
-  const [draggableEvents, setDraggableEvents] = useLocalStorage<
-    DraggableEvent[]
-  >("draggable-events", []);
-
   const [allEvents, setAllEvents] = useState<Event[]>([]);
   const [showModal, setShowModal] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
@@ -118,24 +100,6 @@ export default function Tester() {
     });
   };
 
-  // Function to handle input change
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    // Update the state with the new input value
-    setEventInput(e.target.value);
-  };
-
-  // Function to handle form submission
-  const handleEventSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const event = {
-      id: new Date().getTime(),
-      title: eventInput,
-    };
-    setDraggableEvents([...draggableEvents, event]);
-    // Reset the input field
-    setEventInput("");
-  };
-
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setAllEvents([...allEvents, newEvent]);
@@ -180,32 +144,7 @@ export default function Tester() {
           />
         </div>
 
-        <div
-          id="draggable-element"
-          className="ml-8 border-2 p-2 mt-[72px] rounded-md lg:h-1/2 bg-violet-50">
-          <form className="space-y-2 mb-5" onSubmit={handleEventSubmit}>
-            <input
-              type="text"
-              name="event"
-              onChange={(e) => handleInputChange(e)}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none"
-            />
-            <button
-              type="submit"
-              className="focus:outline-none text-white bg-violet-700 w-full hover:bg-violet-800 focus:ring-4 focus:ring-violet-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2">
-              Add Event
-            </button>
-          </form>
-          <div className="font-bold text-lg text-center">Drag Event</div>
-          {events.map((draggableEvent) => (
-            <div
-              className="fc-event cursor-pointer border-2 p-1 m-2 w-full rounded-md ml-auto text-center bg-white"
-              title={draggableEvent.title}
-              key={draggableEvent.id}>
-              {draggableEvent.title}
-            </div>
-          ))}
-        </div>
+        <DragEvents />
       </div>
       <AddEventModal
         showModal={showModal}
---
+++ components/DragEvents.tsx
@@ -0,0 +1,67 @@
+"use client";
+import useLocalStorage from "@/hooks/useLocalStorage";
+import { useState } from "react";
+
+export interface DraggableEvent {
+  id: number;
+  title: string;
+}
+
+export default function DragEvents() {
+  const [eventInput, setEventInput] = useState<string>("");
+  const [draggableEvents, setDraggableEvents] = useLocalStorage<
+    DraggableEvent[]
+  >("draggable-events", []);
+
+  // Function to handle input change
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    // Update the state with the new input value
+    setEventInput(e.target.value);
+  };
+
+  // Function to handle form submission
+  const handleEventSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (eventInput === "") return;
+
+    const event = {
+      id: new Date().getTime(),
+      title: eventInput,
+    };
+    setDraggableEvents([...draggableEvents, event]);
+    // Reset the input field
+
+    setEventInput("");
+  };
+  return (
+    <>
+      <div
+        id="draggable-element"
+        className="ml-8 border-2 p-2 mt-[72px] rounded-md lg:h-1/2 bg-violet-50">
+        <form className="space-y-2 mb-5" onSubmit={handleEventSubmit}>
+          <input
+            type="text"
+            name="event"
+            onChange={(e) => handleInputChange(e)}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none"
+          />
+          <button
+            type="submit"
+            className="focus:outline-none text-white bg-violet-700 w-full hover:bg-violet-800 focus:ring-4 focus:ring-violet-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2">
+            Add Event
+          </button>
+        </form>
+        <div className="font-bold text-lg text-center">Drag Event</div>
+        {draggableEvents.map((draggableEvent) => (
+          <div
+            className="fc-event cursor-pointer border-2 p-1 m-2 w-full rounded-md ml-auto text-center bg-white"
+            title={draggableEvent.title}
+            key={draggableEvent.id}>
+            {draggableEvent.title}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
