b8c2f1a60d54037e3b225bac480cd94ffef542aa
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-11-23 17:20:00 +0300

    Changed font from Roboto to Geist

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
-
-const roboto = Roboto({
-  weight: ["400"],
-  subsets: ["latin"],
-});
+import { GeistSans } from "geist/font/sans";
 
 export const metadata: Metadata = {
   title: "EventSync",
@@ -19,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={GeistSans.className}>{children}</body>
     </html>
   );
 }
